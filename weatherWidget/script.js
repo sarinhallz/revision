dfc7c80f4762bd77245f3bfcd253f47f46847039
Sarah Cruz <141264235+sarinhallz@users.noreply.github.com>
2023-09-20 16:10:41 -0300

    atividade feita para a fixação do conteúdo JavaScript (WeatherWidget)

--- weatherWidget/script.js
+++ weatherWidget/script.js
@@ -1,3 +1,4 @@
+/* cria uma variável "dadElement" e a atribui ao elemento pai presente no html */
 const dadElement = document.getElementById("dad-element");
 
 /* meu objeto javascript usado para a representação do meu card html */
@@ -23,11 +24,12 @@ const cardWeather = {
     ]
 }
 
+// criação de uma lista de objetos representando cards de diferentes cidades mundiais
 const cards = [
     {
         image: "img/bangkok.jpg",
         city: "BANGKOK",
-        temp: "60°",
+        temp: "28°",
         listWeek: [
             {
                 day: "MON",
@@ -49,7 +51,7 @@ const cards = [
     {
         image: "img/cairo.jpg",
         city: "CAIRO",
-        temp: "60°",
+        temp: "29°",
         listWeek: [
             {
                 day: "MON",
@@ -71,7 +73,7 @@ const cards = [
     {
         image: "img/dubai.jpg",
         city: "DUBAI",
-        temp: "60°",
+        temp: "35°",
         listWeek: [
             {
                 day: "MON",
@@ -93,7 +95,7 @@ const cards = [
     {
         image: "img/moscou.jpg",
         city: "MOSCOU",
-        temp: "60°",
+        temp: "20°",
         listWeek: [
             {
                 day: "MON",
@@ -115,7 +117,7 @@ const cards = [
     {
         image: "img/london.jpg",
         city: "LONDON",
-        temp: "60°",
+        temp: "17°",
         listWeek: [
             {
                 day: "MON",
@@ -137,7 +139,7 @@ const cards = [
     {
         image: "img/oslo.jpg",
         city: "OSLO",
-        temp: "60°",
+        temp: "16°",
         listWeek: [
             {
                 day: "MON",
@@ -159,7 +161,7 @@ const cards = [
     {
         image: "img/ottawa.jpg",
         city: "OTTAWA",
-        temp: "60°",
+        temp: "18°",
         listWeek: [
             {
                 day: "MON",
@@ -181,7 +183,7 @@ const cards = [
     {
         image: "img/pequim.jpg",
         city: "PEQUIM",
-        temp: "60°",
+        temp: "16°",
         listWeek: [
             {
                 day: "MON",
@@ -203,7 +205,7 @@ const cards = [
     {
         image: "img/praga.png",
         city: "PRAGA",
-        temp: "60°",
+        temp: "22°",
         listWeek: [
             {
                 day: "MON",
@@ -225,7 +227,7 @@ const cards = [
     {
         image: "img/rio-de-janeiro.jpg",
         city: "RIO DE JANEIRO",
-        temp: "60°",
+        temp: "24°",
         listWeek: [
             {
                 day: "MON",
@@ -245,32 +247,38 @@ const cards = [
     },
 ]
 
+// percorre por cada um dos cards e cria elementos HTML para cada um deles
 cards.forEach((cards) => {
+    // cria um novo elemento HTML, um elemento filho, que representa o card
     const sonElement = document.createElement("div");
+
+    // estiliza o elemento filho
     sonElement.classList.add("w3-card-4");
     sonElement.classList.add("w3-margin");
     sonElement.style.width = "50%"
 
+    // define o conteúdo HTML do card com base nos dados da cidade
     sonElement.innerHTML = `
     <div class="w3-display-container w3-text-white">
         <img src="${cards.image}" alt="" style="width:100%">
-        <div class="w3-xlarge w3-display-bottomleft w3-padding">${cards.city}</div>
+        <div class="w3-xlarge w3-display-bottomleft w3-padding">${cards.city}, ${cards.temp}</div>
     </div>
     <div class="w3-row">
         <div class="w3-third w3-center">
-            <h3>MON</h3>
+            <h3>${cards.listWeek[0].day}</h3>
             <img src="${cards.listWeek[0].image}" alt="sun" style="width:80px">
         </div>
         <div class="w3-third w3-center">
-            <h3>TUE</h3>
+            <h3>${cards.listWeek[1].day}</h3>
             <img src="${cards.listWeek[1].image}" alt="cloud" style="width:80px">
         </div>
         <div class="w3-third w3-center w3-margin-bottom">
-            <h3>WED</h3>
+            <h3>${cards.listWeek[2].day}</h3>
             <img src="${cards.listWeek[2].image}" alt="clouds" style="width:80px">
         </div>
     </div>`
 
-dadElement.appendChild(sonElement);
+    // adiciona o elemento filho ao elemento pai
+    dadElement.appendChild(sonElement);
 
 });
